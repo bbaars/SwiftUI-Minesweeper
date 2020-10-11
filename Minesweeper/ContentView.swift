74e9c9f555a4f09e3a24514e7207e0e0ee940543
Brandon Baars <brandonbaars@gmail.com>
2020-10-11 19:43:47 -0400

    Initial Commit

---
+++ Minesweeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Minesweeper
+//
+//  Created by Brandon Baars on 10/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Brandon Baars on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
